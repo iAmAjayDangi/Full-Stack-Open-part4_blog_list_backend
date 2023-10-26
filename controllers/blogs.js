5159ef9cd556904576a83e3ebc4683eb47e31e3b
Ajay Dangi <ajay_dangi@persistent.com>
2023-10-26 09:43:50 +0530

    Updated update blog router

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -57,26 +57,27 @@ blogsRouter.delete('/:id', middleware.userExtractor, async (request, response, n
         await Blog.findByIdAndRemove(request.params.id)
         response.status(204).end()
     }
-    catch{
+    catch(exception){
         next(exception)
     }
 })
 
-blogsRouter.put('/:id', async (request, response, next) =>{
+blogsRouter.put('/:id', middleware.userExtractor, async (request, response, next) =>{
     const body = request.body
 
     const blog = {
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes
+        likes: body.likes,
+        user: body.user
     }
 
     try{
         const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
         response.json(updatedBlog)
     }
-    catch{
+    catch(exception){
         next(exception)
     }
 
